bf1cbb45c685922056e8ba8e7803ea5fa2a4e6fd
Jakub Kozłowski <kubukoz@gmail.com>
2022-08-10 23:15:29 +0200

    Provide in memory mode

--- src/main/scala/jvs/AppConfig.scala
+++ src/main/scala/jvs/AppConfig.scala
@@ -3,14 +3,14 @@ package jvs
 import cats.implicits._
 import ciris.ConfigValue
 import jvs.http.HttpConfig
-import jvs.persistence.DatabaseConfig
+import jvs.persistence.PersistenceConfig
 
 object AppConfig {
 
   def config[
     F[_]
-  ]: ConfigValue[F, AppConfig] = (HttpConfig.config[F], DatabaseConfig.config[F]).parMapN(apply)
+  ]: ConfigValue[F, AppConfig] = (HttpConfig.config[F], PersistenceConfig.config[F]).parMapN(apply)
 
 }
 
-final case class AppConfig(http: HttpConfig, db: DatabaseConfig)
+final case class AppConfig(http: HttpConfig, persistence: PersistenceConfig)
--- src/main/scala/jvs/persistence/DatabaseConfig.scala
+++ src/main/scala/jvs/persistence/DatabaseConfig.scala
@@ -6,6 +6,24 @@ import com.comcast.ip4s._
 
 import jvs.config.ConfigDecoders._
 
+sealed trait PersistenceConfig extends Product with Serializable
+
+object PersistenceConfig {
+  final case class WithDatabase(db: DatabaseConfig) extends PersistenceConfig
+  case object InMemory extends PersistenceConfig
+
+  def config[F[_]]: ConfigValue[F, PersistenceConfig] = {
+    import ciris._
+
+    val inMemory = env("DB_IN_MEMORY").map(_ => InMemory).widen[PersistenceConfig]
+
+    val withDatabase = DatabaseConfig.config[F].map(WithDatabase(_)).widen[PersistenceConfig]
+
+    inMemory.or(withDatabase)
+  }
+
+}
+
 final case class DatabaseConfig(
   host: Host,
   port: Port,
